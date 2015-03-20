c3e5cf6a26f7331e9cc3a14d9f4de4ae26b8b947
prashantjois <prashant@jois.ca>
2015-03-20 07:39:55 -0600

    Update auth.rb
    missing end statement

--- app/helpers/api/auth.rb
+++ app/helpers/api/auth.rb
@@ -1,15 +1,16 @@
-module Api
+module Api 
   module Auth
 
-  def current_app
-    # @auth.credentials returns an array of http basic auth [username, password]
-    # since we expect a token only, we will read only the first index as the token
-    @current_app ||= App.where(access_token: @auth.credentials[0]).first
-  end
+    def current_app
+      # @auth.credentials returns an array of http basic auth [username, password]
+      # since we expect a token only, we will read only the first index as the token
+      @current_app ||= App.where(access_token: @auth.credentials[0]).first
+    end 
 
-  def authenticate!
-    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    access_denied! unless @auth.provided? && @auth.credentials && @auth.credentials[0] && current_app
-  end
+    def authenticate!
+      @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+      access_denied! unless @auth.provided? && @auth.credentials && @auth.credentials[0] && current_app
+    end 
+  end 
 
 end
